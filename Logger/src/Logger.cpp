976c0e45a7a93067e3108dd9c743e93195020154
Marcin Siemiński <marcin.sieminski@outlook.com>
2022-05-06 08:07:12 +0200

    Add log level logging

--- Logger/src/Logger.cpp
+++ Logger/src/Logger.cpp
@@ -10,9 +10,10 @@ namespace logger
 	Logger* Logger::m_pThis = NULL;
 	ofstream Logger::m_Logfile;
 
+
+
 	Logger::Logger()
-	{
-	}
+	= default;
 
 	Logger* Logger::GetLogger() {
 		if (m_pThis == NULL) {
@@ -44,11 +45,40 @@ namespace logger
 		m_Logfile << sMessage << "\n";
 	}
 
+	void Logger::Error(const std::string& sMessage)
+	{
+		if (m_LogLevel >= LogLevelError)
+		{
+			Log("[ERROR]: " + sMessage);
+		}
+	}
+
+	void Logger::Warning(const std::string& sMessage)
+	{
+		if (m_LogLevel >= LogLevelWarning)
+		{
+			Log("[WARNING]: " + sMessage);
+		}
+	}
+
+	void Logger::Info(const std::string& sMessage)
+	{
+		if (m_LogLevel >= LogLevelInfo)
+		{
+			Log("[INFO]: " + sMessage);
+		}
+	}
+
 	Logger& Logger::operator<<(const string& sMessage)
 	{
 		m_Logfile << "\n" << CurrentDateTime() << ":\t";
 		m_Logfile << sMessage << "\n";
 		return *this;
+	}
+
+	void Logger::SetLevel(int level)
+	{
+		m_LogLevel = level;
 	}
 }
 
--- Logger/src/Logger.h
+++ Logger/src/Logger.h
@@ -14,14 +14,29 @@ namespace logger
 	public:
 		void Log(const std::string& sMessage);
 		void Log(const char* format, ...);
+
+		void Error(const std::string& sMessage);
+		void Warning(const std::string& sMessage);
+		void Info(const std::string& sMessage);
+
 		Logger& operator<<(const std::string& sMessage);
+
 		static Logger* GetLogger();
+
+		void SetLevel(int level);
+
+		const int LogLevelError = 0;
+		const int LogLevelWarning = 1;
+		const int LogLevelInfo = 2;
+
 	private:
 		Logger();
 		Logger(const Logger&) {};
 		static const std::string m_sFileName;
 		static Logger* m_pThis;
 		static std::ofstream m_Logfile;
+
+		int m_LogLevel = LogLevelInfo;
 	};
 }
 
